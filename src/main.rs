f4b1ab8b9d2fcf428731e75805c472eef8c2d41a
Maximilien Pereira <maxperei@z1r5p4.42lyon.fr>
2024-08-19 20:56:28 +0200

    Add wip tree evaluation

--- src/main.rs
+++ src/main.rs
@@ -25,13 +25,31 @@ fn build_node(iterator: &mut Rev<Chars<'_>>) -> Option<TreeNodeRef> {
     }
 }
 
+fn evaluate_node(node: TreeNode) -> bool {
+    match node.val {
+        '|' => return evaluate_node(node.left) | evaluate_node(node.right),
+        '&' => return evaluate_node(node.left) & evaluate_node(node.right),
+        '^' => return evaluate_node(node.left) ^ evaluate_node(node.right),
+        '=' => return evaluate_node(node.left) == evaluate_node(node.right),
+        '!' => return ! evaluate_node(node.left),
+        // '>' ?
+        '1' => return true,
+        '0' => return false,
+        _ => return false,
+    }
+}
+
 fn eval_formula(formula: &str) -> bool {
     let tree = build_node(&mut formula.chars().rev());
-    println!("{tree:?}");
-    true
+    // println!("{tree:?}");
+    if tree.is_some() {
+        evaluate_node(tree)
+    } else {
+        false
+    }
 }
 
 fn main() {
-    let result = eval_formula("110&|df");
+    let result = eval_formula("10|");
     println!("{result}");
 }
