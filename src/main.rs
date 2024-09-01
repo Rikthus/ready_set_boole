f760275ae667b278cf94bdc2c8d5308df7c513fa
Maximilien Pereira <vague.pair@tutamail.com>
2024-09-01 07:31:36 +0200

    Add combinations generation

--- src/main.rs
+++ src/main.rs
@@ -25,9 +25,9 @@ fn check_alpha_order(formula: &str) -> bool {
             found_alpha.push(c);
         }
     }
-    for val in found_alpha.iter() {
-        println!("{}", val);
-    }
+    // for val in found_alpha.iter() {
+    //     println!("{}", val);
+    // }
     for c in alphabet.chars() {
         if !found_alpha.contains(&c) && alpha_in_streak{
             alpha_in_streak = false;
@@ -59,21 +59,39 @@ fn count_nb_variables(formula: &str) -> usize {
    return var_list.len();
 }
 
+fn  fill_comb_vec(comb_vec: &mut Vec<bool>, comb: u32, nb_vars: usize) {
+    if nb_vars == 0 {
+        return;
+    }
+
+    fill_comb_vec(comb_vec, comb >> 1, nb_vars - 1);
+    comb_vec.push(if comb & 1 == 1 {true} else {false});
+}
+
 fn  generate_combinations(formula: &str) -> Vec<Vec<bool>> {
     let nb_vars: usize = count_nb_variables(formula);
     let nb_combs = u32::pow(2, nb_vars.try_into().unwrap());
     let mut curr_comb: u32 = 0;
     let mut comb_list: Vec<Vec<bool>> = Vec::new();
 
-
-    let mut comb = 0;    
     while curr_comb < nb_combs {
-        comb = gray_code(curr_comb);
-        // for i in range nb_vars
+        let mut comb_vec: Vec<bool> = Vec::new();
+
+        fill_comb_vec(&mut comb_vec, curr_comb, nb_vars);
+        comb_list.push(comb_vec);
         curr_comb += 1;
     }
 
     return comb_list;
+}
+
+fn  print_combs(combs: Vec<Vec<bool>>) {
+    for comb in combs {
+        for val in comb {
+            print!("{}", val);
+        }
+        println!("");
+    }
 }
 
 fn generate_truth_table(formula: &str) -> Result<Vec<Vec<bool>>, InvalidFormula> {
@@ -81,13 +99,17 @@ fn generate_truth_table(formula: &str) -> Result<Vec<Vec<bool>>, InvalidFormula>
     if !parse_formula(formula) {
         return Err(InvalidFormula);
     }
-    let combs = generate_combinations(formula);   
+    let combs = generate_combinations(formula);
+    // print_combs(combs);
+    for comb in combs {
+        
+    }
 
     Ok(truth_table)
 }
 
 fn main() {
-    let result = generate_truth_table("BBBAB|");
+    let result = generate_truth_table("AB|");
     if result.is_ok() {
         println!("GOOD");
     } else {
