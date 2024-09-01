b30bd4a13f2f60e502331e489b14efb95331608b
Maximilien Pereira <vague.pair@tutamail.com>
2024-09-01 08:42:46 +0200

    add working truth tables exo, WIP parsing bad formula logic

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
+use std::{iter::Rev, str::Chars};
 
+type TreeNodeRef = Box<TreeNode>;
+#[derive(Debug, Clone)]
+pub struct TreeNode {
+    val: char,
+    left: Option<TreeNodeRef>,
+    right: Option<TreeNodeRef>,
+}
+
+#[derive(Debug)]
+struct UndefinedBehavior;
 
 #[derive(Debug)]
 struct InvalidFormula;
@@ -86,12 +97,114 @@ fn  generate_combinations(formula: &str) -> Vec<Vec<bool>> {
 }
 
 fn  print_combs(combs: Vec<Vec<bool>>) {
+    let nb_vars = combs[0].len();
+
+    for (i, alpha) in "ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars().enumerate() {
+        if i + 1 == nb_vars {
+            break;
+        }
+        print!("| {} ", alpha);
+    }
+    println!("| = |");
+
+    for i in 0..nb_vars {
+        print!("|---");
+    }
+    println!("|");
     for comb in combs {
         for val in comb {
-            print!("{}", val);
+            print!("| {} ", val as i32);
         }
-        println!("");
+        println!("|");
     }
+}
+
+fn build_node(iterator: &mut Rev<Chars<'_>>) -> Option<TreeNodeRef> {
+    if let Some(val) = iterator.next() {
+        let mut left = Default::default();
+        let mut right = Default::default();
+        if val == '&' || val == '|' || val == '^' || val == '=' || val == '>' {
+            left = build_node(iterator);
+            right = build_node(iterator);
+        } else if val == '!' {
+            left = build_node(iterator);
+        }
+        let node = TreeNode { val, left, right };
+        Some(Box::from(node))
+    } else {
+        None
+    }
+}
+
+fn evaluate_node(node: Option<TreeNodeRef>) -> Result<bool, UndefinedBehavior> {
+    if node.is_some() {
+        let unwrapped_node = node.unwrap();
+        match unwrapped_node.val {
+            '|' => {
+                return Ok(
+                    evaluate_node(unwrapped_node.left)? | evaluate_node(unwrapped_node.right)?
+                )
+            }
+            '&' => {
+                return Ok(
+                    evaluate_node(unwrapped_node.left)? & evaluate_node(unwrapped_node.right)?
+                )
+            }
+            '^' => {
+                return Ok(
+                    evaluate_node(unwrapped_node.left)? ^ evaluate_node(unwrapped_node.right)?
+                )
+            }
+            '=' => {
+                return Ok(
+                    evaluate_node(unwrapped_node.left)? == evaluate_node(unwrapped_node.right)?
+                )
+            }
+            '>' => {
+                return Ok(
+                    !evaluate_node(unwrapped_node.left)? | evaluate_node(unwrapped_node.right)?
+                )
+            }
+            '!' => return Ok(!evaluate_node(unwrapped_node.left)?),
+            '1' => return Ok(true),
+            '0' => return Ok(false),
+            _ => return Err(UndefinedBehavior),
+        }
+    } else {
+        Err(UndefinedBehavior)
+    }
+}
+
+fn get_value(c: char, comb: &Vec<bool>) -> char {
+    for (i, alpha) in "ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars().enumerate() {
+        if c == alpha {
+            let mut j = 0;
+            for value in comb {
+                if j == i {
+                    if *value == true {
+                        return '1';
+                    } else {
+                        return '0';
+                    }
+                }
+                j += 1;
+            }
+        }
+    }
+    '0'
+}
+
+fn substitute_formula(formula: &str, comb: &Vec<bool>) -> String {
+    let mut comb_formula: String = String::new();
+
+    for c in formula.chars() {
+        if c.is_alphabetic() {
+            comb_formula.push(get_value(c, comb));
+        } else {
+            comb_formula.push(c);
+        }
+    }
+    comb_formula
 }
 
 fn generate_truth_table(formula: &str) -> Result<Vec<Vec<bool>>, InvalidFormula> {
@@ -100,18 +213,25 @@ fn generate_truth_table(formula: &str) -> Result<Vec<Vec<bool>>, InvalidFormula>
         return Err(InvalidFormula);
     }
     let combs = generate_combinations(formula);
-    // print_combs(combs);
-    for comb in combs {
-        
-    }
+    for mut comb in combs {
+        let comb_formula = substitute_formula(formula, &comb);
+        let tree_root = build_node(&mut comb_formula.chars().rev());
+        let result = evaluate_node(tree_root);
 
+        if result.is_ok() {
+            comb.push(result.unwrap());
+            truth_table.push(comb);
+        } else {
+            return Err(InvalidFormula);
+        }
+    }
     Ok(truth_table)
 }
 
 fn main() {
-    let result = generate_truth_table("AB|");
+    let result = generate_truth_table("AB&C|");
     if result.is_ok() {
-        println!("GOOD");
+        print_combs(result.unwrap());
     } else {
         println!("BAD");
     }
